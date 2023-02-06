c187f46ae76291a192b292a5248d233470ec0e0f
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-06 23:56:43 +0100

    Change port listening message

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,4 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
