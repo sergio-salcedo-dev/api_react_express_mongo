819e4d3321d3b78bc0ef0f2e14ac225b2f1539a0
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-06 23:59:49 +0100

    Rename params

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@ import app from "./server.mjs";
 
 const PORT = 80;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (request, response) => {
+  response.send("Hello World!");
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
