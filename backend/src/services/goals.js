e47723065c31afba3864862103da9ebadeb1efc1
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-07 18:02:59 +0100

    Use service responseHandler.js

--- backend/src/services/goals.js
+++ backend/src/services/goals.js
@@ -1,15 +1,19 @@
+import {
+  handleFailedResponse,
+  handleSuccessfulResponse,
+} from "./responseHandler.js";
+
 export const getGoals = async (request, response) => {
   try {
     console.log("TRYING TO FETCH GOALS");
 
     const goals = [{ id: 1, text: "Hello World" }];
+    const mappedGoals = goals.map((goal) => ({
+      id: goal.id,
+      text: goal.text,
+    }));
 
-    const result = response.status(200).json({
-      goals: goals.map((goal) => ({
-        id: goal.id,
-        text: goal.text,
-      })),
-    });
+    const result = handleSuccessfulResponse(response, { goals: mappedGoals });
 
     console.log("FETCHED GOALS");
 
@@ -18,6 +22,6 @@ export const getGoals = async (request, response) => {
     console.error("ERROR FETCHING GOALS");
     console.error(error.message);
 
-    return response.status(500).json({ message: "Failed to load goals." });
+    return handleFailedResponse(response, { message: "Failed to load goals." });
   }
 };
