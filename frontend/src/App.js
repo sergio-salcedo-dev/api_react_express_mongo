b125b0c45ac4352fe8b97bb7c96c26e0eeb48c8e
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-07 23:10:27 +0100

    Add react custom function App() & frontend components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,123 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import GoalInput from "./components/goals/GoalInput";
+import CourseGoals from "./components/goals/CourseGoals";
+import ErrorAlert from "./components/UI/ErrorAlert";
+import { ENDPOINT_GOAL, ENDPOINT_GOALS } from "./endpoints";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+function App() {
+  const [loadedGoals, setLoadedGoals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(function () {
+    async function fetchData() {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(ENDPOINT_GOALS);
+        console.log(response);
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message || "Fetching the goals failed.");
+        }
+
+        setLoadedGoals(responseData.goals);
+      } catch (error) {
+        setError(
+          error.message ||
+            "Fetching goals failed - the server responded with an error."
+        );
+      }
+      setIsLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  async function addGoalHandler(goalText) {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(ENDPOINT_GOAL, {
+        method: "POST",
+        body: JSON.stringify({
+          text: goalText,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error(responseData.message || "Adding the goal failed.");
+      }
+
+      setLoadedGoals((prevGoals) => {
+        return [
+          {
+            id: responseData.goal.id,
+            text: goalText,
+          },
+          ...prevGoals,
+        ];
+      });
+    } catch (error) {
+      setError(
+        error.message ||
+          "Adding a goal failed - the server responded with an error."
+      );
+    }
+    setIsLoading(false);
   }
+
+  async function deleteGoalHandler(goalId) {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(`${ENDPOINT_GOAL}/${goalId}`, {
+        method: "DELETE",
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error(responseData.message || "Deleting the goal failed.");
+      }
+
+      setLoadedGoals((prevGoals) =>
+        prevGoals.filter((goal) => goal.id !== goalId)
+      );
+    } catch (error) {
+      setError(
+        error.message ||
+          "Deleting the goal failed - the server responded with an error."
+      );
+    }
+
+    setIsLoading(false);
+  }
+
+  return (
+    <section className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>UI (React) that connect to API (Express)</h2>
+      </header>
+      <main className="App-intro">
+        {error && <ErrorAlert errorText={error} />}
+        <GoalInput onAddGoal={addGoalHandler} />
+        {!isLoading && (
+          <CourseGoals goals={loadedGoals} onDeleteGoal={deleteGoalHandler} />
+        )}
+      </main>
+    </section>
+  );
 }
 
 export default App;
---
+++ frontend/src/endpoints.js
@@ -0,0 +1,3 @@
+const LOCALHOST = "http://localhost";
+export const ENDPOINT_GOAL = `${LOCALHOST}/goal`;
+export const ENDPOINT_GOALS = `${LOCALHOST}/goals`;
