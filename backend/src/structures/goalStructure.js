bebd6771e57d32d740e5f53a1d1f41ef60516750
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-20 22:12:58 +0100

    Add default params value for GoalStructure

--- backend/src/structures/goalStructure.js
+++ backend/src/structures/goalStructure.js
@@ -1,13 +1,13 @@
 export default class GoalStructure {
   /** @param {object} params */
   constructor(params = {}) {
-    /**@type {string} */
-    this.id = params.id;
+    /**@type {?string} */
+    this.id = params.id ?? null;
 
-    /**@type {string} */
-    this.text = params.text;
+    /**@type {?string} */
+    this.text = params.text ?? null;
 
     /**@type {boolean} */
-    this.isCompleted = params.isCompleted;
+    this.isCompleted = params.isCompleted ?? false;
   }
 }
