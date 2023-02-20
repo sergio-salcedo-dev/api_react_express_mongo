6518dc4899c1f2a8a78effe9bfa98ff6f519fc29
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-20 22:12:04 +0100

    Add Logger.backendError()

--- backend/src/services/logger.js
+++ backend/src/services/logger.js
@@ -6,4 +6,16 @@ export default class Logger {
   static error(message) {
     console.error(message);
   }
+
+  /**
+   * @param {object} error
+   * @param {string} errorMessage
+   */
+  static backendError(error, errorMessage = "backendError") {
+    Logger.error(errorMessage);
+    Logger.error("--------- ERROR --------------");
+    Logger.error(error);
+    Logger.error("--------- END ERROR -----------");
+    Logger.error("");
+  }
 }
