5b42f405240387e96bda3e1b5679865dc26fa6da
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-20 16:40:29 +0100

    Add property isCompleted to goal model

--- backend/src/models/goal.js
+++ backend/src/models/goal.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const goalSchema = new Schema({ text: String });
+const goalSchema = new Schema({
+  isCompleted: {
+    type: Schema.Types.Boolean,
+    default: false,
+  },
+  text: String,
+});
 
 const GoalModel = mongoose.model("Goal", goalSchema);
 
