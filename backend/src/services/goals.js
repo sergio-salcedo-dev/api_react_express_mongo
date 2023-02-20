d1297ec0de04307d2b02ecca1486aab39e6f2b20
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-20 16:43:17 +0100

    Handle not found GET goal/id

--- backend/src/services/goals.js
+++ backend/src/services/goals.js
@@ -1,6 +1,7 @@
 import {
   handleCreatedResponse,
   handleFailedResponse,
+  handleNotFoundResponse,
   handleSucceededResponse,
   handleUnprocessableEntityResponse,
 } from "./responseHandler.js";
@@ -34,17 +35,28 @@ export const getGoal = async (request, response) => {
   try {
     Logger.log("TRYING TO FETCH GOAL");
 
-    const goalId = request.params.id;
+    const goalId = request?.params?.id;
 
-    if (!goalId || parseInt(goalId) < 1) {
+    const hex = /([0-9a-fA-F]{24})/g;
+    const isValidId = hex.test(goalId || "");
+
+    if (!goalId || parseInt(goalId) < 1 || !isValidId) {
       Logger.log("Invalid goal id");
 
       return handleUnprocessableEntityResponse(response, {
-        message: "Invalid goal id",
+        message: "Invalid goal id. Example: 63f38adf22ae09032e52bfc0",
       });
     }
 
-    const goal = await Goal.findOne({ _id: goalId });
+    const goal = await Goal.find({ _id: goalId }).limit(1);
+
+    if (!goal || !Object.keys(goal).length) {
+      Logger.log(`Goal with id = ${goalId} not found`);
+
+      return handleNotFoundResponse(response, {
+        message: "Goal not found",
+      });
+    }
 
     const result = handleSucceededResponse(response, { goal });
 
@@ -90,6 +102,65 @@ export const insertGoal = async (request, response) => {
     Logger.error(err.message);
 
     return handleFailedResponse(response, { message: "Failed to save goal." });
+  }
+};
+
+export const updateGoal = async (request, response) => {
+  Logger.log("TRYING TO UPDATE GOAL");
+
+  /** @type {string | undefined} */
+  const goalText = request?.body?.text;
+  const goalId = request?.params?.id;
+
+  const hex = /([0-9a-fA-F]{24})/g;
+  const isValidId = hex.test(goalId || "");
+
+  if (!goalId || parseInt(goalId) < 1 || !isValidId) {
+    Logger.log("Invalid goal id");
+
+    return handleUnprocessableEntityResponse(response, {
+      message: "Invalid goal id. Example: 63f38adf22ae09032e52bfc0",
+    });
+  }
+
+  if (!goalText || goalText.trim().length === 0) {
+    Logger.log("Invalid input: the goal cannot be empty");
+
+    return handleUnprocessableEntityResponse(response, {
+      message: "Invalid goal input text.",
+    });
+  }
+
+  // const goal = new Goal({ text: goalText });
+  // const goal = await Goal.findOne({ _id: goalId });
+  const goal = await Goal.find({ _id: goalId }).limit(1);
+
+  if (!goal || !Object.keys(goal).length) {
+    Logger.log(`Goal with id = ${goalId} not found`);
+
+    return handleNotFoundResponse(response, {
+      message: "Goal not found",
+    });
+  }
+
+  try {
+    await Goal.updateOne(goal.id, { text: goalText });
+
+    const result = handleSucceededResponse(response, {
+      message: "Goal updated",
+      goal: { id: goal.id, text: goalText, isCompleted: goal.isCompleted },
+    });
+
+    Logger.log("UPDATED GOAL");
+
+    return result;
+  } catch (err) {
+    Logger.error("ERROR UPDATING GOALS");
+    Logger.error(err.message);
+
+    return handleFailedResponse(response, {
+      message: "Failed to update goal.",
+    });
   }
 };
 
