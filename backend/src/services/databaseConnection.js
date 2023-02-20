af6ef73615f32e7c2ac8704fa4134c6d77490a52
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-20 19:15:48 +0100

    Refactor databaseConnection.js

--- backend/src/services/databaseConnection.js
+++ backend/src/services/databaseConnection.js
@@ -2,32 +2,35 @@ import mongoose from "mongoose";
 import app from "../server.mjs";
 import Logger from "./logger.js";
 
+const onDatabaseConnect = (error) => {
+  if (error) {
+    Logger.error("FAILED TO CONNECT TO MONGODB");
+    Logger.error(error);
+  } else {
+    Logger.log("CONNECTED TO MONGODB!!");
+    startServer();
+  }
+};
+
+const startServer = () => {
+  const PORT = process.env.APP_PORT;
+  app.listen(PORT, () => Logger.log(`Server started on port ${PORT}`));
+};
+
 /** @param {boolean} connectToMongoBD */
 export const connectToDatabase = (connectToMongoBD = true) => {
-  const PORT = process.env.PORT;
-
   if (connectToMongoBD) {
-    const URI = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_DOCKER_SERVICE_NAME}:${process.env.MONGODB_PORT}/${process.env.APP_NAME}?authSource=admin`;
-
+    const URI = `${process.env.MONGODB_URI_CONNECTION}/${process.env.APP_NAME}?authSource=admin`;
     mongoose.connect(
-      // `mongodb://user:secret@mongodb:27017/app-goals?authSource=admin`,
       URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       },
-      (error) => {
-        if (error) {
-          console.error("FAILED TO CONNECT TO MONGODB");
-          console.error(error);
-        } else {
-          console.log("CONNECTED TO MONGODB!!");
-          app.listen(PORT, () => Logger.log(`Server started on port ${PORT}`));
-        }
-      }
+      onDatabaseConnect
     );
   } else {
-    console.log("NOT CONNECTED TO MONGODB!!");
-    app.listen(PORT, () => Logger.log(`Server started on port ${PORT}`));
+    Logger.log("NOT CONNECTED TO MONGODB!!");
+    startServer();
   }
 };
